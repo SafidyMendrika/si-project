e8f13392d1d0d12c443cd2163f54feb8307e6dc5
ETU001849 <118928190+TahiryRamaroson@users.noreply.github.com>
2023-07-10 23:03:18 +0300

    list pack complet tsy tafavoaka

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -14,10 +14,21 @@ class Menu extends CI_Model
       return $query->result_array();
     }
 
+    public function convertKg($kilo){
+        $kcal_1kg = 7700;
+        return $kcal_1kg * $kilo;
+      }
+
+      public function getNbJour($totalKcal, $sportKcal){
+        $answer = $totalKcal/$sportKcal;
+        return ceil(abs($answer));
+      }
+
     public function get($id_pack){
         $list_pack = $this->getByPack($id_pack);
         foreach ($list_pack as $element) {
             $element['total_kcal'] = $this->getTotalKcal($id_pack);
+            $element['duration'] = ($this->getTotalKcal($id_pack) + $this->convertKg($element['weight_to_operate']))
         }
         return $list_pack;
     }
