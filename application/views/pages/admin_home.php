69de6dae47f1f3254e002b7ecfe4f767843a4e7c
ETU001849 <118928190+TahiryRamaroson@users.noreply.github.com>
2023-07-11 09:36:02 +0300

    dashboard

--- application/views/pages/admin_home.php
+++ application/views/pages/admin_home.php
@@ -49,12 +49,102 @@
     <!-- Navbar -->
 </header>
 
-
 <main style="margin-top: 58px">
     <div class="container pt-4">
-
+        <div class="row">
+            <div class="col-md-4">
+                <h4>Nombre d'utilisateur</h4>
+                    <p>22 personnes</p>
+            </div>
+            <div class="col-md-4">
+                <h4>Programme de perte de poids</h4>
+                    <p>10 personnes</p>
+            </div>
+            <div class="col-md-4">
+                <h4>Programme de prise de poids</h4>
+                    <p>10 personnes</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <div class="chart" style="width: 500px;">
+                <h4>Nombre de transactions par mois</h4>
+                    <canvas id="myChart1"></canvas>
+                </div>
+            </div>
+            <div class="col-md-2"></div>
+            <div class="col-md-5">
+                <div class="chart" style="width: 500px;">
+                <h4>Chiffre d'affaire par mois</h4>
+                    <canvas id="myChart2"></canvas>
+                </div>
+            </div>
+        </div>
     </div>
+    
+            
 </main>
+
+<script src="<?= base_url("assets/js/chart-js/node_modules/chart.js/dist/chart.umd.js") ?>"></script>
+<script>
+    const ctx1 = document.getElementById('myChart1');
+    const ctx2 = document.getElementById('myChart2');
+
+    new Chart(ctx1, {
+        type: 'line',                //bar, line, pie, radar, doughnut, bubble
+        data: {
+            labels: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin'],
+            datasets: [{
+                label: '1 Euro en ariary',
+                data: [4325, 4350, 4700, 4575, 4450, 4600],
+                borderWidth: 1
+            },{
+                label: '1 dollar en ariary',
+                data: [3500, 3675, 4000, 3900, 4100, 4125],
+                borderWidth: 1
+            },{
+                label: '1 Yen en ariary',
+                data: [28, 34, 37, 34, 28, 30],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
+
+    new Chart(ctx2, {
+        type: 'bar',                //bar, line, pie, radar, doughnut, bubble
+        data: {
+            labels: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin'],
+            datasets: [{
+                label: '1 Euro en ariary',
+                data: [4325, 4350, 4700, 4575, 4450, 4600],
+                borderWidth: 1
+            },{
+                label: '1 dollar en ariary',
+                data: [3500, 3675, 4000, 3900, 4100, 4125],
+                borderWidth: 1
+            },{
+                label: '1 Yen en ariary',
+                data: [28, 34, 37, 34, 28, 30],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
+    
+</script>
 
 <!-- MDB -->
 <script type="text/javascript" src="<?= base_url("assets/js/mdb.min.js") ?>"></script>
